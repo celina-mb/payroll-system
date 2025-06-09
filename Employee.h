76fd78f28d6eb55a8a6e8ebdfcfda26f5bbcdcda
Celina MB <103222853+celina-mb@users.noreply.github.com>
2025-06-09 01:07:01 -0600

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -1,5 +1,4 @@
-#ifndef EMPLOYEE_H
-#define EMPLOYEE_H
+#pragma once
 
 #include <string>
 
@@ -12,12 +11,10 @@ class Employee{
 
     public:
     //constructor
-    Boss(std::string name, std::string id, int workingDays, float baseSalary);
+    Employee(std::string name, std::string id, int workingDays, float baseSalary);
     std::string getName();
     std::string getID();
 
     virtual float calculatePay() = 0;
     virtual void showInfo() = 0;
 };
-
-#endif
