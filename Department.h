9412a559fb45bd66f15cf76467ada787982c9889
Celina MB <103222853+celina-mb@users.noreply.github.com>
2025-06-10 20:27:16 -0600

    Update Department.h

--- Department.h
+++ Department.h
@@ -11,7 +11,10 @@ class Department{
     public:
     Department(std::string name, std::string id);
 
+    std::string getDepartmentName();
     std::string getDepartmentID();
+    std::vector<Employee*> getEmployees();
+
     void addEmployee(Employee* emp);
     void removeEmployee(std::string empID);
     float calculateDepartmentPay();
