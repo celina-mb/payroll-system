d9d286d16eddd9e8b06aedd57ebba28ffc00fcb8
Celina MB <103222853+celina-mb@users.noreply.github.com>
2025-06-10 20:50:16 -0600

    Update Company.cpp

--- Company.cpp
+++ Company.cpp
@@ -1,26 +1,47 @@
-#include "Boss.h"
+#include "Company.h"
 
-//constructor
-Boss::Boss(std::string name, std::string id, int workingDays, float baseSalary, float bonus): Employee(name, id, workingDays, baseSalary) {
-    this-> bonus = bonus;
+Company::Company(std::string name){
+    this-> name = name;
 }
 
-//setter for Bonus as it might change over time
-void Boss::setBonus(float qty){
-    bonus = qty;
+//this is necesary for displaying the company's payroll in main
+std::string Company::getName(){
+    return name;
 }
 
-/*no need for a getter method here, as all the information will be displayed through a string 
-also, no external classes for which the attributes are private will need access to the private
-attributes*/
-
-//method overloading
-float Boss::calculateSalary(float tax){ //receives tax as a float number, not percentage
-    return (baseSalary+bonus)*tax;
+void Company::addDepartment(Department* d){
+    departments.push_back(d);
 }
 
-//method overriding
-std::string Boss::displayInfo(){
-    std::string msg = "Boss : "+ name + " |ID: "+ id+ " |Working Days: "+ std::to_string(workingDays)+ " |Base Salary: "+ std::to_string(baseSalary)+ "| Pay: "+  std::to_string(calculateSalary(0.15));
-    return msg;
+//removes a department by looping through the vector's elements until it finds the one that matches the function's input
+void Company::removeDepartment(std::string dID){
+    for (auto i = departments.begin(); i!=departments.end(); ++i){
+        if ((*i)->getDepartmentID() == dID){
+            departments.erase(i);
+            return;
+        }
+    }
+}
+
+//cummulative for all salaries within departments (departments within a company)
+float Company::calculateCompanySalaries(){
+    float salaries = 0.0f;
+    for (Department* d: departments){
+        salaries += d->calculateDepartmentPay();
+    }
+    return salaries;
+}
+
+//uses the displayInfo method found in department, with a for loop
+std::string Company::displayInfo(){
+    std::string aux = std::string("-------------------------\n") + "COMPANY INFORMATION\n" + std::string("-------------------------\n")+ "Company Name: "+ name+ " \n"+"Total Departments: "+std::to_string(departments.size()) + "\n";
+    for (Department* d : departments){
+        aux += d->displayInfo() + "\n" + std::string("-------------------------\n");
+    }
+    return aux;
+}
+
+//operator overloading
+bool Company::operator==(Company& other){
+    return (this-> departments.size()==other.departments.size());
 }
